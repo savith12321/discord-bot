76e2558935df71358ae2d80ec9fcff4f77df94e2
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-05-25 10:41:05 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(message, args, guildprefix) {
         colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
         const helpEmbed = new Discord.MessageEmbed()
-            .setTitle("help for moring-bot")
+            .setTitle("help for heat")
             .setAuthor(`Prefix = ${guildprefix}`)
             .setThumbnail("https://media.discordapp.net/attachments/834658912422592553/835052092766158899/Untitled_design_1.png")
             .setURL(`https://discord.com/oauth2/authorize?client_id=804622611828047872&scope=bot&permissions=8589934591`)
