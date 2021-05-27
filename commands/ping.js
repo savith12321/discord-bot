56366ec2311da0960654b54074af6756ded510d7
savith nishitha <isavith.nishitha@gmail.com>
2021-05-27 14:43:17 +0530

    first commit

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.channel.send('pong!');
+        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 }
