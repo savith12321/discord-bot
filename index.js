4b8d73eba5fe00a64129a38606ad338c85f1303c
savith nishitha <isavith.nishitha@gmail.com>
2021-05-27 12:56:09 +0530

    first commit

--- index.js
+++ index.js
@@ -191,8 +191,8 @@ client.on("guildCreate", async guild => {
                                 prefix: "lol ",
                             }).save();
                           }
-						                    channel.send(embed)
                         });
+                        channel.send(embed)
                         found = 1;
                     }
                 }
