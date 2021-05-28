b003e4ebc617b0604d80767f2517d2cd11abc8e2
savith nishitha <isavith.nishitha@gmail.com>
2021-05-28 13:23:28 +0530

    sky lineeeeeeeee

--- commands/restart.js
+++ commands/restart.js
@@ -2,10 +2,10 @@ const config = require('../config');
 module.exports = {
     name: 'restart',
     description: "restart bots conection to discord api",
-    execute(message, args, client){
+    async execute(message, args, client){
         let done = message.channel.send('Resetting...')
         .then(message => client.destroy())
         .then(() => client.login(config.token))
-        .then(() => done.edit("Restarting hasbean done :)"))
+        .then(() => await done.edit("Restarting hasbean done :)"))
     }
 }
