3e56feed08e6445a78b9d95e161b7c87a01c7e2a
savith nishitha <isavith.nishitha@gmail.com>
2021-05-28 10:34:43 +0530

    sky lineeeeeeeee

--- index.js
+++ index.js
@@ -47,6 +47,28 @@ client.once('ready', async() => {
     client.user.setActivity(`[lol help] | Watching ${servers} servers | v1.0`, { type: "LISTENING" });
     console.log(`bot is ready ${servers} ${users}`);
 });
+client.on('guildMemberAdd', async(member, guild) => {
+    welcomeSchema.findOne({ guildId: member.guild.id }, async(err, data) => {
+        if (!data) return;
+        const user = member.user;
+        const channel = member.guild.channels.cache.get(data.channelId);
+        if (data.channelId === "") return;
+        let welcomeimg = await canva.welcome(member, { link: "http://www.beach-backgrounds.com/wallpapers/view-from-the-hill-on-the-monaco-wallpaper-1920x600-461.jpg" })
+        const userEmbed = new Discord.MessageEmbed()
+            .setTitle(`Welcome ${user.username} to ${member.guild.name} the discord server`)
+            .setThumbnail(`${user.displayAvatarURL()}`)
+            .setColor("RANDOM")
+            .addField(`About you`, `\n USER TAG: ${user} \n USER NO: ${member.guild.memberCount}`)
+            .addField(`About us`, `\n SERVER NAME: ${member.guild.name} \n TOTAL MEMBERS WITH YOU: ${member.guild.memberCount}`)
+        const attachment = new Discord.MessageAttachment(
+            welcomeimg,
+            "welcome-image.png"
+        );
+        channel.send(`Hey ${user} welcome to our server **${member.guild.name}** \n`);
+        channel.send(userEmbed);
+        channel.send(attachment);
+    })
+});
 
 client.on('message', async message => {
     prefixSchema.findOne({ guildId: message.guild.id }, async(err, data) => {
@@ -147,29 +169,6 @@ client.on('message', async message => {
     });
 
 });
-client.on('guildMemberAdd', async(member, guild) => {
-    welcomeSchema.findOne({ guildId: member.guild.id }, async(err, data) => {
-        if (!data) return;
-        const user = member.user;
-        const channel = member.guild.channels.cache.get(data.channelId);
-        if (data.channelId === "") return;
-        let welcomeimg = await canva.welcome(member, { link: "http://www.beach-backgrounds.com/wallpapers/view-from-the-hill-on-the-monaco-wallpaper-1920x600-461.jpg" })
-        const userEmbed = new Discord.MessageEmbed()
-            .setTitle(`Welcome ${user.username} to ${member.guild.name} the discord server`)
-            .setThumbnail(`${user.displayAvatarURL()}`)
-            .setColor("RANDOM")
-            .addField(`About you`, `\n USER TAG: ${user} \n USER NO: ${member.guild.memberCount}`)
-            .addField(`About us`, `\n SERVER NAME: ${member.guild.name} \n TOTAL MEMBERS WITH YOU: ${member.guild.memberCount}`)
-        const attachment = new Discord.MessageAttachment(
-            welcomeimg,
-            "welcome-image.png"
-        );
-        channel.send(`Hey ${user} welcome to our server **${member.guild.name}** \n`);
-        channel.send(userEmbed);
-        channel.send(attachment);
-    })
-});
-
 client.on("guildCreate", async guild => {
     let found = 0;
     guild.channels.cache.map(async channel => {
