d805b84a42577e2257db98e8da6de93887056b6c
savith nishitha <isavith.nishitha@gmail.com>
2021-05-28 12:59:17 +0530

    sky lineeeeeeeee

--- commands/restart.js
+++ commands/restart.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'restart',
     description: "restart bots conection to discord api",
     execute(message, args, client){
-        channel.send('Resetting...')
+        message.channel.send('Resetting...')
         .then(msg => client.destroy())
         .then(() => client.login(config.token));
     }
