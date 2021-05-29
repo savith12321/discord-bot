92581a4ea1411cc3bb62ce7aecdac704faad9020
savith <isavith.nishitha@gmail.com>
2021-05-29 10:11:37 +0530

    sky lineeeeeeeee

--- commands/restart.js
+++ commands/restart.js
@@ -6,6 +6,10 @@ module.exports = {
         let done = message.channel.send('Resetting...')
         client.destroy()
         client.login(config.token);
+		const servers = await client.guilds.cache.size;
+		const users = await client.users.cache.siz;
+		client.user.setActivity(`[lol help] | Watching ${servers} servers | v1.0`, { type: "LISTENING" });
+		console.log(`bot is ready ${servers} ${users}`);
         message.channel.send("Reloaded");
         return;
 
