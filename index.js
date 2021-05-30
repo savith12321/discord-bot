420756472d92e015d060a6cd37038bc00e76869c
savith <isavith.nishitha@gmail.com>
2021-05-30 14:45:07 +0530

    sky lineeeeeeeee

--- index.js
+++ index.js
@@ -211,3 +211,14 @@ client.on("guildCreate", async guild => {
     });
 
 })
+
+client.on('guildDelete', guild => {
+  const userEmbed = new Discord.MessageEmbed()
+  userEmbed.setColor("RANDOM")
+  userEmbed.setTitle(`i got removed from a server`)
+  userEmbed.addField(`server name:`, `${guild.name}`)
+  userEmbed.addField("guild id:", `${guild.id}`)
+  userEmbed.setTimestamp()
+  owner = client.users.cache.get("801752135850655755");
+  owner.send(userEmbed);
+});
