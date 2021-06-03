c588478c9a80679fb524f4fbe95e53ffcd7b3137
savith <isavith.nishitha@gmail.com>
2021-06-03 10:04:00 +0530

    sky lineeeeeeeee

--- commands/robot.js
+++ commands/robot.js
@@ -1,34 +1,16 @@
 var unirest = require("unirest");
 const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
 module.exports = {
-    name: 'robot',
+    name: 'mchead',
     cooldown: 10,
     description: "this is a robot command!",
     execute(message, args, client) {
-        var req = unirest("GET", "https://robohash.p.rapidapi.com/index.php");
-
-        req.query({
-            "text": "mashape"
-        });
-
-        req.headers({
-            "x-rapidapi-key": "3bf5242cbcmshb9d36e4c02b7119p113cb8jsn1dce95748840",
-            "x-rapidapi-host": "robohash.p.rapidapi.com",
-            "useQueryString": true
-        });
-
-
-        req.end(function(res) {
-            if (res.error) throw new Error(res.error);
-
-            console.log(res.body);
-            const embed = new Discord.MessageEmbed()
-                .setTitle(`beep boop beep boop?`)
-                .setColor(`RANDOM`)
-                .setImage(res.body.imageUrl)
-                .setURL(res.body.imageUrl);
-            message.channel.send(embed);
-        });
-
+        if (!args[0]) return message.channel.send("please type a name of a minecraft playe :(");
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`beep boop beep boop ? `)
+            .setColor(`RANDOM `)
+            .setImage(`https://www.mc-heads.net/avatar/${args[0]}`)
+            .setURL(res.body.imageUrl);
+        message.channel.send(embed);
     }
 }
