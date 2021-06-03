b4acb668d755a180e5d318e00d778d91e2d798fa
savith <isavith.nishitha@gmail.com>
2021-06-03 07:51:41 +0530

    sky lineeeeeeeee

--- commands/garve.js
+++ commands/garve.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'grave',
     cooldown: 1,
     description: "sends grave pic with crop by the messages aouthors pfp",
-    async execute(message, args, client) {
+    execute(message, args, client) {
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad'];
         const Embed = new Discord.MessageEmbed()
@@ -13,14 +13,10 @@ module.exports = {
         Embed.setTimestamp();
 
 
-        let button = new MessageButton()
-            .setLabel("Vote")
-            .setStyle("url")
-            .setEmoji("🍔")
-            .setURL("https://discord-buttons.js.org")
-        message.channel.send('Pizza', {
-            component: button,
-            embed: Embed
-        });
+        let btn = new MessageButton()
+            .setStyle('green')
+            .setLabel('open image on web')
+            .setURL(encodeURI(`https://vacefron.nl/api/grave?user=${Member.user.displayAvatarURL({ format: "png" })}`));
+        message.channel.send('Pizza', { component: btn, embed: Embed });
     }
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
+require('discord-buttons')(client);
 const config = require('./config');
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 const mongoose = require('mongoose');
-["commnad_handler","event_handler"].forEach(handler =>{
+["commnad_handler", "event_handler"].forEach(handler => {
     require(`./handlers/${handler}`)(Discord, client)
 })
 mongoose
