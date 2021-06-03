1d6160fad490a02b6589582a724c4f6e1a7a4445
savith <isavith.nishitha@gmail.com>
2021-06-03 07:21:37 +0530

    sky lineeeeeeeee

--- commands/garve.js
+++ commands/garve.js
@@ -1,20 +1,19 @@
 const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
-const disbut = require('discord-buttons');
+const { MessageButton } = require('discord-buttons');
 module.exports = {
     name: 'grave',
     cooldown: 1,
     description: "sends grave pic with crop by the messages aouthors pfp",
     async execute(message, args, client) {
-        disbut(client)
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-        colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
+        colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad'];
         const Embed = new Discord.MessageEmbed()
-        Embed.setColor(colors[Math.floor(Math.random() * colors.length)])
-        Embed.setImage(encodeURI(`https://vacefron.nl/api/grave?user=${Member.user.displayAvatarURL({ format: "png" })}`))
+        Embed.setColor(colors[Math.floor(Math.random() * colors.length)]);
+        Embed.setImage(encodeURI(`https://vacefron.nl/api/grave?user=${Member.user.displayAvatarURL({ format: "png" })}`));
         Embed.setTimestamp();
 
 
-        let button = new disbut.MessageButton()
+        let button = new MessageButton()
             .setLabel("Vote")
             .setStyle("url")
             .setEmoji("🍔")
