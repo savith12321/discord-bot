4a13ccaf261f320c174f152c3498e739586e92b6
savith <isavith.nishitha@gmail.com>
2021-06-03 07:11:14 +0530

    sky lineeeeeeeee

--- commands/garve.js
+++ commands/garve.js
@@ -1,10 +1,11 @@
 const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
-const { MessageButton } = require('discord-buttons');
+const disbut = require('discord-buttons');
 module.exports = {
     name: 'grave',
     cooldown: 1,
     description: "sends grave pic with crop by the messages aouthors pfp",
-    async execute(message, args) {
+    async execute(message, args, client) {
+        disbut(client)
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
         colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
         const Embed = new Discord.MessageEmbed()
@@ -13,7 +14,7 @@ module.exports = {
         Embed.setTimestamp();
 
 
-        let button = new MessageButton()
+        let button = new disbut.MessageButton()
             .setLabel("Vote")
             .setStyle("url")
             .setEmoji("🍔")
