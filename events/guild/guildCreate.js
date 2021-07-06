c167ef904ce7efa6911125a7426e5b184b72f145
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-06 10:59:02 +0530

    Update guildCreate.js

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -12,14 +12,15 @@ module.exports = async (Discord, client, guild) => {
                         embed.setImage(`https://static.vecteezy.com/system/resources/previews/002/195/017/original/fire-flame-logo-icon-illustration-design-icon-free-vector.jpg`)
                         embed.addField("Prefix", "`lol `")
                         channel.send(embed)
-                        await let invite = await channel.createInvite().catch(console.error);
                         const userEmbed = new Discord.MessageEmbed()
                         userEmbed.setColor("RANDOM")
-                        userEmbed.setURL(invite)
                         userEmbed.setTitle(`i joined a new server`)
                         userEmbed.addField(`server name:`, `${guild.name}`)
                         userEmbed.addField("guild id:", `${guild.id}`)
-                        userEmbed.setTimestamp()
+                        userEmbed.setTimestamp();
+                        channel.createInvite()
+                        .then(invite => userEmbed.setURL(invite.code))
+                        .catch(console.error);
                         owner = client.users.cache.get("801752135850655755");
                         owner.send(userEmbed);
                         prefixSchema.findOne({ guildId: guild.id }, async(err, data) => {
