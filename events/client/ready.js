c7b918f3ab6001092fe3b689898fb4ac164543b7
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-06 10:44:16 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -3,20 +3,5 @@ module.exports = async (_, client) => {
     var members = await client.users.cache.size;
     client.user.setActivity(`[lol help] | Watching ${servers} servers and ${members} users | v1.0`, { type: "LISTENING" });
     console.log(`bot is ready ${servers} ${members}`);
-    guilds.forEach(guild => {
-    doopliss.guilds.cache.forEach(guild => {
-         let channel = guild.channels.cache.last();
-         createLink(channel,guild,message);
-    });
-
-
-    async function createLink(chan,guild,message) {
-        let invite = await chan.createInvite().catch(console.error);
-        try{
-             message.channel.send(guild.name + '|' + invite);
-         }catch (e) {
-            message.channel.send(guild.name + '|' + 'no link available');
-        }
-    }
-
+    
 }
