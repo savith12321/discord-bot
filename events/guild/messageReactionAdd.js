7c38c18706d795bcd5b826a7a562e434db79a3c6
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-09 10:13:32 +0530

    Update messageReactionAdd.js

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -1,6 +1,6 @@
 module.exports = async (Discord, client, messageReaction, user) => {
     if(!messageReaction.message.channel.type === "dm") return;
-    message = client.channels.cache.get(user.id).messages.fetch(messageReaction.message.id);
+    message = client.channels.cache.get(user.id).messages.cache.get(messageReaction.message.id);
     console.log(client.user.id)
     if(message.author.id == client.user.id && messageReaction.emoji.name == "❌"){
         messageReaction.message.delete()
