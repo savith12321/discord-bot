ccca657275052bd101d7b9141ba3509f5ebc5ae9
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-11 10:48:43 +0530

    Update messageReactionAdd.js

--- events/guild/messageReactionAdd.js
+++ events/guild/messageReactionAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (Discord, client, messageReaction, user) => {
-    if(messageReaction.emoji.name == "❌" && messageReaction.message.guild.id == null){
+    if(messageReaction.emoji.name == "❌" && messageReaction.message.guild == null){
         messageReaction.message.delete()
     }
 }
