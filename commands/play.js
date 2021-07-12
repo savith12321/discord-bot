6232e93da5f14d9271fe61aad5a5168c79ef5ef0
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-12 13:18:39 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -33,9 +33,7 @@ module.exports = {
             var server = servers[message.guild.id];
 
             server.queue.push(video.url);
-            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
                 play(connection, message);
-            });
             async function play(connection, url) {
                 var server = servers[message.guild.id];
 
