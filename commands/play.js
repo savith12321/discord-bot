63732b5fd8f122e0985c2c948afebdada172566f
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-12 08:49:30 +0530

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'play',
+    cooldown:3,
+    description: "this is a ban command!",
+    async execute(message, args){
+      message.channel.send("This music command is under devolopment :musical_note:")
+    }
+}
