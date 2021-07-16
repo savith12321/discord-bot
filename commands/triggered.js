56b8cb5c9c674ac2a2e01513304f0f5c67d7179b
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-16 15:09:24 +0530

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -6,11 +6,6 @@ module.exports = {
     execute(message, args){
       const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
       colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
-      const Embed = new Discord.MessageEmbed()
-        .setColor(colors[Math.floor(Math.random() * colors.length)])
-        .setImage(encodeURI(`https://some-random-api.ml/canvas/triggered?avatar=${Member.user.displayAvatarURL({ format: "png" })}`))
-        .setTimestamp();
-
-      return message.channel.send(Embed);
+      return message.channel.send(encodeURI(`https://some-random-api.ml/canvas/triggered?avatar=${Member.user.displayAvatarURL({ format: "png" })}`));
     }
 }
