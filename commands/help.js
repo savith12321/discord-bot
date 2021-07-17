203275c5b4ce56ad84f913c030850a11b0fa89ae
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-17 16:00:18 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
             .setURL(`https://discord.com/oauth2/authorize?client_id=846615243673042954&scope=bot&permissions=8589934591`)
             .addField(`About:`, "`help` - sends our commands\n`info` - bot's info")
             .addField(`Misc:`, "`setwelcome` - set a welcome message\n`removewelcome` - remove welcome message\n`setprefix` - change your guild prefix\n`feedback` - send us a feedback\n`bug` - report a bug")
-            .addField(`Fun:`, "`heaven <@user>` - create a image of a heaven\n`iamspeed <@user>` - create a image of a iamspeed\n`cat` - send a random image of cat\n`dog` - send a random image of a dog\n`quote`- send a random quote\n`rps` - play rock paper scissors\n`google` - Search on google! \n `ytsearch` - search on youtube! \n `grave` - send's a grave image")
+            .addField(`Fun:`, "`heaven <@user>` - create a image of a heaven\n`iamspeed <@user>` - create a image of a iamspeed\n`cat` - send a random image of cat\n`dog` - send a random image of a dog\n`quote`- send a random quote\n`rps` - play rock paper scissors\n`google` - Search on google! \n `ytsearch` - search on youtube! \n `grave` - send's a grave image \n `wasted` - make a GTA wasted image \n `lyrics` - send lyrics of a message!")
             .addField(`Moderation:`, "`user-info <@user>` - get <@user>'s info\n`clear <number 1-100>` - purge command!\n`kick <@user>` - kick someone!\n`ban <@user>`- ban someone!")
             .addField(`Memy:`, "`meme` - random meme\n`chuck-joke` - random chuck joke")
             .addField(`Ticket:`, "`ticket` - create a ticket")
