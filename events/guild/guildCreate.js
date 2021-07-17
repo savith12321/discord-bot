dc5723fa9d6c88b7c8cd7baa35ea933778015fbc
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-07-17 14:08:17 +0530

    Update guildCreate.js

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -25,7 +25,7 @@ module.exports = async (Discord, client, guild) => {
                         .then(invite => userEmbed.setURL(invite.url))
                         .catch(console.error);
                         owner = client.users.cache.get("801752135850655755");
-                        owner2 = client.users.cache.get("801752135850655755");
+                        owner2 = client.users.cache.get("832511674392510464");
                         owner.send(userEmbed);
                         owner2.send(userEmbed);
                         console.log(guild.name)
