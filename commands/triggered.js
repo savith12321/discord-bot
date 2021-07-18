8d05110c54a907c4885d5aa0f996005408389e22
savith nishitha <isavith.nishitha@gmail.com>
2021-07-18 15:29:00 +0530

    Update triggered.js

--- commands/triggered.js
+++ commands/triggered.js
@@ -4,8 +4,8 @@ module.exports = {
     cooldown:1,
     description: "this is a ping command!",
     execute(message, args, client){
-        const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-        let link = `https://some-random-api.ml/canvas/triggered/?avatar=${Member.avatarURL({ format: 'png'})}`
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        let link = `https://some-random-api.ml/canvas/triggered/?avatar=${member.user.avatarURL({ format: 'png'})}`
         const attachment = new MessageAttachment(link, 'triggered.gif');
         const embed = new MessageEmbed()
           .setTitle(`triggered`)
