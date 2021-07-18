37a4dbee0e6b46a1d0b221e648130b7bc154ab44
savith nishitha <isavith.nishitha@gmail.com>
2021-07-18 19:51:27 +0530

    why

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (_, client) => {
     var servers = await client.guilds.cache.size;
     var members = await client.users.cache.size;
-    client.user.setActivity(`[lol help] | Watching ${servers} servers and ${members} users | v1.0`, { type: "LISTENING" });
+    client.user.setActivity(`[lol help] | Watching ${servers} servers and ${members} users | v1.1`, { type: "LISTENING" });
     console.log(`bot is ready ${servers} ${members}`);
-    
+
 }
