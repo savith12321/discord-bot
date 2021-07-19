021545712aa735d8d14cd356db8b7ce5c5a46e41
savith nishitha <isavith.nishitha@gmail.com>
2021-07-19 17:09:15 +0530

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    cooldown:10,
+    description: "this is a ping command!",
+    execute(message, args, client){
+        const embed = new Discord.MessageEmbed();
+        yourEmbed.addField("Title here, no hyperlinks allowed", "Main text here, so you can put a hyperlink here [![Discord Bots](https://top.gg/api/widget/846615243673042954.svg)](https://top.gg/bot/846615243673042954)");
+
+    }
+}
