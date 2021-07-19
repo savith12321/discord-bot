4961f2479d472aeffe396220921016928d4cce25
savith nishitha <isavith.nishitha@gmail.com>
2021-07-19 16:43:59 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -2,6 +2,7 @@ const commnad_handler = require('../../handlers/commnad_handler');
 const cooldowns = new Map();
 const prefixSchema = require('../../models/prefix-schema')
 module.exports = async (Discord, client, message) => {
+
     await prefixSchema.findOne({ guildId: message.guild.id }, async(err, data) => {
         if (!data) {
             await new prefixSchema({
@@ -13,38 +14,38 @@ module.exports = async (Discord, client, message) => {
         }else{
             guildprefix = data.prefix;
             if (message.guild == null) guildprefix = "lol ";
-            if (!message.content.startsWith(guildprefix) || message.author.bot) return;
+            if (!message.content.shift().toLowerCase().startsWith(guildprefix) || message.author.bot) return;
             const args = message.content.slice(guildprefix.length).split(/ +/);
             const cmd = args.shift().toLowerCase();
-            
+
 
             const command = client.commands.get(cmd);
 
             if(message.author.id == "801752135850655755"){
                 if(command) command.execute(message, args, client, guildprefix);
                 return;
-            } 
+            }
 
             if(!cooldowns.has(command.name)){
                 cooldowns.set(command.name, new Discord.Collection());
             }
-        
+
             const current_time = Date.now();
             const time_stamps = cooldowns.get(command.name);
             const cooldown_amount = (command.cooldown) * 1000;
             if(time_stamps.has(message.author.id) ){
                 const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-        
+
                 if(current_time < expiration_time){
                     const time_left = (expiration_time - current_time) / 1000;
-        
+
                     return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name} command (if command cooldowns are too long please feedback me with 'lol feedback <content>')`);
                 }
             }
             time_stamps.set(message.author.id, current_time);
             setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
             if(command) command.execute(message, args, client, guildprefix);
-        } 
+        }
     });
 
 }
