2768660b33f537e3fe9fea9c88d457a7b4956598
savith nishitha <isavith.nishitha@gmail.com>
2021-07-19 18:32:15 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -5,8 +5,8 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args, client){
         const embed = new Discord.MessageEmbed();
-        embed.addField("Vote us on Top.gg!", "[VOTE NOW!](https://top.gg/bot/846615243673042954/vote)");
-        embed.addField("Vote us on discordbotlist.com","[VOTE NOW](https://discordbotlist.com/bots/heat/upvote)")
+        embed.addField("Vote us on Top.gg!", "`[VOTE NOW!](https://top.gg/bot/846615243673042954/vote)`");
+        embed.addField("Vote us on discordbotlist.com","`[VOTE NOW!](https://discordbotlist.com/bots/heat/upvote)`")
         embed.setColor("RANDOM")
         embed.setTitle("Vote For Heat :)")
         embed.setTimestamp();
