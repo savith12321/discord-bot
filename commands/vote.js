f071f8729aff5f2e67baca0d77684d19cb1c2d2d
savith nishitha <isavith.nishitha@gmail.com>
2021-07-19 17:17:49 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -5,7 +5,8 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args, client){
         const embed = new Discord.MessageEmbed();
-        yourEmbed.addField("Title here, no hyperlinks allowed", "Main text here, so you can put a hyperlink here [![Discord Bots](https://top.gg/api/widget/846615243673042954.svg)](https://top.gg/bot/846615243673042954)");
+        embed.addField("Title here, no hyperlinks allowed", "Main text here, so you can put a hyperlink here [![Discord Bots](https://top.gg/api/widget/846615243673042954.svg)](https://top.gg/bot/846615243673042954)");
+        message.channel.send(embed)
 
     }
 }
