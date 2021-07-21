29bf803e526a1ca49e63f041cf7fc1aded62908f
savith nishitha <isavith.nishitha@gmail.com>
2021-07-21 15:33:13 +0530

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -4,12 +4,15 @@ module.exports = {
     cooldown:10,
     description: "this is a ping command!",
     execute(message, args, client){
-        if(!message.author.id == '801752135850655755') return message.channel.send(":warning: This is a owner only command!");
+        if(message.author.id == '801752135850655755'){
         console.log(message.author.id)
         var msg = args.join(" ")
         var id = msg.split("|")
         var user = client.users.cache.get(id[0]);
         if(!user) return message.channel.send(":warning: could not fetch that user");
         user.send(id[1]);
+      }else{
+        return message.channel.send(":warning: This is a owner only command!");
+      }
     }
 }
