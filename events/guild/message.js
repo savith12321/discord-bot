7f11cba2659aa7a2b9232e67e633075a6cbd06b6
savith nishitha <isavith.nishitha@gmail.com>
2021-07-21 17:18:30 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -13,7 +13,7 @@ module.exports = async (Discord, client, message) => {
             method: 'POST',
             url: 'https://robomatic-ai.p.rapidapi.com/api.php',
             headers: {
-              'Accept':'application/json'
+              'Accept':'application/json',
               'content-type': 'application/x-www-form-urlencoded',
               'x-rapidapi-key': '3bf5242cbcmshb9d36e4c02b7119p113cb8jsn1dce95748840',
               'x-rapidapi-host': 'robomatic-ai.p.rapidapi.com'
