c166222fc86c2574316d6cbe0068a6b677049645
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-01 08:22:22 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
             .setTitle("help for heat")
             .setAuthor(`Prefix = ${guildprefix}`)
             .setThumbnail("https://static.vecteezy.com/system/resources/previews/002/195/017/original/fire-flame-logo-icon-illustration-design-icon-free-vector.jpg")
-            .setURL(`https://discord.com/oauth2/authorize?client_id=846615243673042954&scope=bot&permissions=8589934591`)
+            .setURL(`https://discord.com/oauth2/authorize?client_id=874131652920614942&scope=bot&permissions=8589934591`)
             .addField(`About:`, "`help` - sends our commands\n`info` - bot's info")
             .addField(`Misc:`, "`setwelcome` - set a welcome message\n`removewelcome` - remove welcome message\n`setprefix` - change your guild prefix\n`feedback` - send us a feedback\n`bug` - report a bug")
             .addField(`Fun:`, "`heaven <@user>` - create a image of a heaven\n`iamspeed <@user>` - create a image of a iamspeed\n`cat` - send a random image of cat\n`dog` - send a random image of a dog\n`quote`- send a random quote\n`rps` - play rock paper scissors\n`google` - Search on google! \n `ytsearch` - search on youtube! \n `grave` - send's a grave image \n `wasted` - make a GTA wasted image \n `lyrics <name of a song>` - send lyrics of a song! \n `binaryencode <text>` - encode a text to binary \n `binarydecode <binary code>` - decode binary to text")
