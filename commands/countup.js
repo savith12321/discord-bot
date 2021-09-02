8b190a74774117f1300fd36bdbc00c132c125fe1
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-02 16:56:24 +0530

    Create countup.js

--- commands/countup.js
+++ commands/countup.js
@@ -6,7 +6,7 @@ module.exports = {
         if (!args[0])return message.channel.send(":warning: quary cant be null");
         if (args[0].isNaN)return message.channel.send(":warning: you should give a number");
         const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-        
+        //hi
         let msg = await message.channel.send(args[0])
         for(let i = args[0]; i >= 0; i--){
             await delay(1000)
