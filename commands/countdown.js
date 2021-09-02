74536e02981db11d85eece3110075b59ee661b3e
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-02 15:48:26 +0530

    Create countdown.js

---
+++ commands/countdown.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'countup',
+    cooldown:10,
+    description: "this is a ping command!",
+    execute(message, args, client){
+        if (!args[0])return message.channel.send(":warning: quary cant be null");
+        if (args[0].isNaN)return message.channel.send(":warning: you should give a number");
+        
+        let message2= message.channel.sned(args[0])
+        for(let i = args[0]; i <= 0; i--){
+            message2.edit(i);
+        }
+    }
+}
