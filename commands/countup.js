6733d51d6963c9f475a4465541e312a3aa2a96a0
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-02 16:15:50 +0530

    Update countup.js

--- commands/countup.js
+++ commands/countup.js
@@ -6,7 +6,7 @@ module.exports = {
         if (!args[0])return message.channel.send(":warning: quary cant be null");
         if (args[0].isNaN)return message.channel.send(":warning: you should give a number");
         
-        await let message2= message.channel.send(args[0])
+        await let message2 = message.channel.send(args[0])
         for(let i = args[0]; i <= 0; i--){
           await message2.edit(i);
         }
