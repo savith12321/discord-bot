f2c0de185f8f4c3f88e94182e4c30710f14e1ed5
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-02 17:49:24 +0530

    Update and rename snake.js to kangaroo.js

---
+++ commands/kangaroo.js
@@ -0,0 +1,26 @@
+const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const axios = require("axios");
+module.exports = {
+    name: 'kangaroo',
+    cooldown:10,
+    description: "sends randome dog pics",
+    execute(message, args) {
+        colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
+        axios
+            .get('https://some-random-api.ml/animal/kangaroo')
+            .then((res) => {
+                console.log("RES:", res.data[0].url)
+                const cat = res.data[0].url
+                const embed = new MessageEmbed()
+                    .setTitle("kangaroo ?")
+                    .setImage(res.data[0].image)
+                    .setTimestamp()
+                    .setColor(colors[Math.floor(Math.random() * colors.length)])
+                    .setFooter("For kangaroo <3 only")
+
+                message.channel.send(embed);
+
+            })
+
+    }
+}
