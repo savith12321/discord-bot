06d131207aa7261d4c65745d65cbb60fb2cc6174
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-02 17:10:53 +0530

    Update countdown.js

--- commands/countdown.js
+++ commands/countdown.js
@@ -5,6 +5,7 @@ module.exports = {
     async execute(message, args, client){
         if (!args[0])return message.channel.send(":warning: quary cant be null");
         if (args[0].isNaN)return message.channel.send(":warning: you should give a number");
+        if(args[0] < 1)return message.channel.send(":warning: the number you enter must be grater thane 1");
         const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
         //hi
         let msg = await message.channel.send(args[0])
