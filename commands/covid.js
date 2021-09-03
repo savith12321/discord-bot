2b90fe2ba90526a8b0f42da8227130e67a61c936
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-03 13:28:04 +0530

    Create covid.js

---
+++ commands/covid.js
@@ -0,0 +1,34 @@
+//https://coronavirus-19-api.herokuapp.com/countries/sri%20lanka2/
+const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const axios = require("axios");
+module.exports = {
+    name: 'covid',
+    cooldown:10,
+    description: "sends randome dog pics",
+    execute(message, args) {
+        let countury = = args.join("%20");
+        if (!args[0]) countury = 'world'
+        let url = `https://coronavirus-19-api.herokuapp.com/countries/`;
+        axios
+            .get('https://some-random-api.ml/animal/kangaroo')
+            .then((res) => {
+                console.log("RES:", res.data.image)
+                const embed = new MessageEmbed()
+                    .setTitle(res.data.country)
+                    .addField("cases:", res.data.cases)
+                    .addField("today cases:", res.data.todayCases)
+                    .addField("deaths", res.data.deaths)
+                    .addField("today deaths", res.data.todayDeaths)
+                    .addField("recovered", res.data.recovered)
+                    .addField("active", res.data.active)
+                    .addField("total tests", res.data.totalTests)
+                    .setTimestamp()
+                    .setColor("RANDOM")
+                    .setFooter("Stay Safe :)")
+
+                message.channel.send(embed);
+
+            })
+
+    }
+}
