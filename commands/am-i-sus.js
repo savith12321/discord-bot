c1ca47cae82fd153f6d064decfe16bc111cd6789
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-04 13:01:23 +0530

    Update am-i-sus.js

--- commands/am-i-sus.js
+++ commands/am-i-sus.js
@@ -8,12 +8,14 @@ module.exports = {
       const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
       colors = ['blue','brown','cyan','darkgreen','lime','orange','pink','purple','red','white','yellow']
       imposter = ['true', 'false']
+      am_i_the_imposter = imposter[Math.floor(Math.random() * imposter.length
       const Embed = new Discord.MessageEmbed()
         .setColor()
         .setColor("RANDOM")
-        .setImage(encodeURI(`https://vacefron.nl/api/ejected?name=${Member.user.displayAvatarURL({ format: "png" })}&impostor=${imposter[Math.floor(Math.random() * imposter.length)]}&crewmate=${colors[Math.floor(Math.random() * colors.length)]}`))
+        .setImage(encodeURI(`https://vacefron.nl/api/ejected?name=${Member.user.username({ format: "png" })}&impostor=${am_i_the_imposter}&crewmate=${colors[Math.floor(Math.random() * colors.length)]}`))
         .setTimestamp();
-
+      if(am_i_the_imposter){Embed.setTitle("YES")}
+      else{Embed.setTitle("NO")}
       return message.channel.send(Embed);
     }
 }
