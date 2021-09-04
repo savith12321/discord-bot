a3ae1cae85c360e7c57b23d9014d4afc74590a57
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-04 15:17:51 +0530

    Update am-i-sus.js

--- commands/am-i-sus.js
+++ commands/am-i-sus.js
@@ -8,7 +8,7 @@ module.exports = {
       const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
       colors = ['blue','brown','cyan','darkgreen','lime','orange','pink','purple','red','white','yellow']
       imposter = ['true', 'false']
-      am_i_the_imposter = imposter[Math.floor(Math.random() * imposter.length];
+      am_i_the_imposter = imposter[Math.floor(Math.random() * imposter.length)];
       const Embed = new Discord.MessageEmbed()
         .setColor()
         .setColor("RANDOM")
