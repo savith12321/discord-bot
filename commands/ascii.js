06d914f036a9342c893588b7b73e070871d83882
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-06 14:05:57 +0530

    Create ascii.js

---
+++ commands/ascii.js
@@ -0,0 +1,18 @@
+const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const axios = require("axios");
+module.exports = {
+    name: 'ascii',
+    cooldown:10,
+    description: "sends randome dog pics",
+    execute(message, args) {
+        let countury = args.join("%20");
+        if (!args[0]) message.channel.send(":warning: quary can be null");
+        let url = `https://artii.herokuapp.com/make?text=${countury}`;
+        axios
+            .get(url)
+            .then((res) => {
+                message.channel.send(res.data);
+            })
+
+    }
+}
