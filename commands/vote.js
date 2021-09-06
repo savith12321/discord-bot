0d89deba9c3b4ccfbec50022621961b59778ed7a
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-06 11:30:35 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message, args, client){
         const embed = new Discord.MessageEmbed();
         embed.addField("Vote us on Top.gg!", "[`VOTE NOW!`](https://top.gg/bot/846615243673042954/vote)");
-        embed.addField("Vote us on discordbotlist.com","[`VOTE NOW!`](https://discordbotlist.com/bots/heat/upvote)")
+        embed.addField("Vote us on discordbotlist.com","[`VOTE NOW!`](https://discordbotlist.com/bots/heat-5417/upvote)")
         embed.setColor("RANDOM")
         embed.setTitle("Vote For Heat :)")
         embed.setTimestamp();
