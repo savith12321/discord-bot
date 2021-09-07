9b6676a9b32cd370e288bb2d058d46a4727979b5
savith12321 <73011842+savith12321@users.noreply.github.com>
2021-09-07 14:19:56 +0530

    Update monkey.js

--- commands/monkey.js
+++ commands/monkey.js
@@ -1,26 +1,35 @@
-const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
-const axios = require("axios");
+var unirest = require("unirest");
+const fetch = require('node-fetch');
 module.exports = {
-    name: 'monkey',
+    name: 'chuck-joke',
     cooldown:10,
-    description: "sends randome cat pics",
-    execute(message, args){
-      colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
-      axios
-          .get('https://api.monkedev.com/attachments/monkey')
-          .then((res) => {
-              console.log("RES:", res.data[0].url)
-              //const cat = res.data[0].url
-              const embed = new MessageEmbed()
-                  .setTitle("Meaw")
-                  .setImage(res.data.url)
-                  .setTimestamp()
-                  .setColor(colors[Math.floor(Math.random() * colors.length)])
-                  .setFooter("For cat lovers only")
+    description: "send a random chuck-joke",
+    execute(message, args) {
 
-              message.channel.send(embed);
 
-          })
+        var req = unirest("GET", "https://api.monkedev.com/attachments/monkey");
+
+        req.query({
+            "format": "json"
+        });
+
+        req.headers({
+            "key": "c06OgzD85phncFvg6ys5qSH1q"
+        });
+
+
+        req.end(function(res) {
+            if (res.error) throw new Error(res.error);
+
+            console.log(res.body.joke);
+           const embed = new MessageEmbed()
+          .setTitle("Meaw")
+          .setImage(res.data.url)
+          .setTimestamp()
+          .setColor(colors[Math.floor(Math.random() * colors.length)])
+          .setFooter("For cat lovers only")
+           message.channel.send(embed);
+        });
 
     }
 }
