7af255a3ef1eba6b8ad1c6b1dfd608b29ab583bc
savith nishitha <isavith.nishitha@gmail.com>
2021-09-09 15:49:43 +0530

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -5,26 +5,26 @@ module.exports = {
     cooldown:1,
     description: "this is a ping command!",
     async execute(message, args, client){
-        let profileData;
         try {
-          profileData = await profileSchema.findOne({ userID: message.author.id });
-          if (!profileData) {
-            let profile = await profileSchema.create({
-              UserID: message.author.id,
-              UserName: message.author.username,
-              wollet: 500,
-              bank: 0,
+            await profileSchema.findOne({ userID: message.author.id }, async (err, data) =>{
+                if (!data) {
+                    let profile = await profileSchema.create({
+                    UserID: message.author.id,
+                    UserName: message.author.username,
+                    wollet: 500,
+                    bank: 0,
+                    });
+                    profile.save();
+                }else{
+                let embed = new Discord.MessageEmbed()
+                .setTitle("Balance")
+                .setColor("RANDOM")
+                .setTimestamp()
+                .addField("Wollet:", data.wollet)
+                .addField("Bank", data.bank);
+                message.channel.send(embed);
+                }
             });
-            profile.save();
-          }else{
-          let embed = new Discord.MessageEmbed()
-          .setTitle("Balance")
-          .setColor("RANDOM")
-          .setTimestamp()
-          .addField("Wollet:", profileData.wollet)
-          .addField("Bank", profileData.bank);
-          }
-          message.channel.send(embed);
         } catch (err) {
           console.log(err);
         }
