f26f4463b0bafede8c07d52c71172fc03471791a
savith12321 <isavith.nishitha@gmail.com>
2021-09-11 17:46:29 +0530

    sky lineeeeeeeee

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@ git init
 git add *
 git commit -m "sky lineeeeeeeee"
 git branch -M main
-git remote add origin https://github.com/savith12321/moring-bot.git
+git remote add origin https://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@github.com/savith12321/moring-bot.git
 git push -u origin main
