b799bcf4bb837d2d3b428d5206804eb356ccd707
savith nishitha <isavith.nishitha@gmail.com>
2021-09-12 14:52:29 +0530

    ss

--- commands/ban.js
+++ commands/ban.js
@@ -3,16 +3,19 @@ module.exports = {
     cooldown:10,
     description: "this is a ban command!",
     async execute(message, args){
+      let error = false;
       if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Don't wast your time bro **YOU DONT HAVE PERMS TO DO THAT IDIDOT**");
-
       var member = message.mentions.users.first();
       if(member){
           var target = message.guild.members.cache.get(member.id);
           await target.ban().catch(async (err) => {
+            error = true;
             await message.react("❌");
             return message.channel.send(":boom: I could not ban that ban that member see do i have perms or he/she have higher perms thane me")
-          });
-          message.reply(`You **BAN** ${target}`)
+          });          
+          if(!error){
+            message.reply(`You **KICK** ${target}`);
+          }
       }
       else{
         message.reply("Don't be an idiot **MENTION A MEMBER TO BAN IDIOT**");
--- commands/kick.js
+++ commands/kick.js
@@ -3,16 +3,20 @@ module.exports = {
     cooldown:10,
     description: "this is a kick command!",
     async execute(message, args){
+      let error = false;
       if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Don't wast your time bro **YOU DONT HAVE PERMS TO DO THAT IDIDOT**").then(m => m.delete(5000));
 
       var member = message.mentions.users.first();
       if(member){
           var target = message.guild.members.cache.get(member.id);
           await target.kick().catch(async (err) => {
+            error = true;
             await message.react("❌");
-            return message.channel.send(":boom: I could not kick that ban that member see do i have perms or he/she have higher perms thane me")
+            return message.channel.send(":boom: I could not kick that ban that member see do i have perms or he/she have higher perms thane me").then(m => m.delete(5000));
           });
-          message.reply(`You **KICK** ${target}`)
+          if(!error){
+            message.reply(`You **KICK** ${target}`);
+          }
       }
       else{
         message.reply("Don't be an idiot **MENTION A MEMBER TO KICK IDIOT**")
