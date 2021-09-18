9c043177e52b108a6f3076ed591cf20514f50942
savith nishitha <isavith.nishitha@gmail.com>
2021-09-18 15:50:27 +0530

    Create vote.js

---
+++ SlashCommands/vote.js
@@ -0,0 +1,25 @@
+const { Client, CommandInteraction } = require("discord.js");
+const {SlashCommandBuilder} = require("@discordjs/builders")
+module.exports = {
+    ...new SlashCommandBuilder().setName("vote").setDescription("vote for us ❣️"),
+    run: async (client, interaction, args) => {
+        const row = new Discord.MessageActionRow().addComponents(
+            new Discord.MessageButton()
+            .setLabel("Vote us on Top.gg")
+            .setURL("https://top.gg/bot/874131652920614942/vote")
+            .setStyle("LINK"),
+
+            new Discord.MessageButton()
+            .setLabel("Vote us on discordbotlist.com")
+            .setStyle("LINK")
+            .setURL("https://discordbotlist.com/bots/heat-5417/upvote"),
+
+            new Discord.MessageButton()
+            .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+            .setLabel("probably not rick roll")
+            .setStyle("LINK")
+
+        );
+        interaction.followUp({content: "❤️ hey you want to vote for us, thank you ❤️", components : [row]})
+    }
+};
