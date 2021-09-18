72debf968f0e73f365f281d5e2aa803d5a981903
savith nishitha <isavith.nishitha@gmail.com>
2021-09-18 14:25:28 +0530

    Update rickroll.js

--- SlashCommands/rickroll.js
+++ SlashCommands/rickroll.js
@@ -3,7 +3,7 @@ const {SlashCommandBuilder} = require("@discordjs/builders")
 module.exports = {
     ...new SlashCommandBuilder().setName("rickroll").setDescription("rick roll some one lol").addUserOption(option => option.setName("user").setDescription("user you want to rick roll").setRequired(true)),
     run: async (client, interaction, args) => {
-        const user  = interaction.options.getString("user");
+        const user  = interaction.options.getUser("user");
 
         interaction.followUp({ content: `rick roll bait sent to ${user}`, ephemeral: true });
     }
