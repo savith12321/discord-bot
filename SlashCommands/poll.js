8fd6107671bacf8c1b9302ed101e313e4b9c3816
savith nishitha <isavith.nishitha@gmail.com>
2021-09-19 13:23:04 +0530

    Create poll.js

---
+++ SlashCommands/poll.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const {SlashCommandBuilder} = require("@discordjs/builders")
+module.exports = {
+    ...new SlashCommandBuilder().setName("poll").setDescription("create a poll :o").addStringOption(option => option.setName("title").setDescription("set a title for your poll").setRequired(true)).addStringOption(option => option.setName("description").setDescription("set a description for your poll").setRequired(true)),
+    run: async (client, interaction, args) => {
+        let pollEmbed = Discord.MessageEmbed()
+        .setTitle(interaction.options.getString("title"))
+        .setDescription(interaction.options.getString("description"))
+        .setTimestamp()
+        .setColor("RANDOM")
+        let msg = await interaction.followUp({ embeds:[pollEmbed]});
+
+        await msg.react("👍️")
+        await msg.react("👎️")
+    }
+};
