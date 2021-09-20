bca4cc02f29b74bbbcf79a5dd4d8db01577ebe7c
savith nishitha <isavith.nishitha@gmail.com>
2021-09-20 15:28:19 +0530

    Update poll.js

--- SlashCommands/poll.js
+++ SlashCommands/poll.js
@@ -14,7 +14,7 @@ const title_and_body = args.join(" ").split('|');
 const { Client, CommandInteraction } = require("discord.js");
 const {SlashCommandBuilder} = require("@discordjs/builders")
 module.exports = {
-    ...new SlashCommandBuilder().setName("ping").setDescription("pong lol").addStringOption(option => option.setName("title").setDescription("Title of your poll").setRequired(true)).addStringOption(option => option.setName("description").setDescription("description of your poll").setRequired(true)),
+    ...new SlashCommandBuilder().setName("poll").setDescription("create a poll :o").addStringOption(option => option.setName("title").setDescription("Title of your poll").setRequired(true)).addStringOption(option => option.setName("description").setDescription("description of your poll").setRequired(true)),
     run: async (client, interaction, args) => {
        let pollEmbed = new Discord.MessageEmbed()
        .setTitle(interaction.options.getString("title"))
