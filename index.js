27d4c678f25bab2fd396cc8a462d634d7318f0c5
savith nishitha <isavith.nishitha@gmail.com>
2021-09-21 14:56:17 +0530

    .

--- index.js
+++ index.js
@@ -6,23 +6,25 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.slashCommands = new Discord.Collection();
 const mongoose = require('mongoose');
+const express = require("express")
+const api = express();
 ["commnad_handler", "event_handler"].forEach(handler => {
     require(`./handlers/${handler}`)(Discord, client)
-})
-
-var log_file_err=fs.createWriteStream('./error.log',{flags:'a'});  
-
-process.on('TypeError', function(err) {
-        log_file_err.write('Caught exception: '+err+"\n");
-        console.log(err);
 });
+
+api.get('/', (req, res) =>{
+    res.send("'Hello':'World!'")
+});
+
 mongoose
     .connect(config.mongosrv, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => {
         console.log("connected to mongo db")
-        client.login(config.token).catch((err) =>{
+        client.login(config.token).then(() =>{
+            api.listen(3000, () => console.log("lisntning on port 3000"))
+        }).catch((err) =>{
             console.log(err)
         });
     }).catch((err) => {
