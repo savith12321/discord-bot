eb33ccb30baf36db0e95737cebd3217d4634da15
savith nishitha <isavith.nishitha@gmail.com>
2021-09-21 11:58:03 +0530

    Update r-nick.js

--- SlashCommands/r-nick.js
+++ SlashCommands/r-nick.js
@@ -9,7 +9,7 @@ module.exports = {
             .get(url)
             .then((res) => {
                 interaction.followUp("```"+res.data[0]+" is your new nick name```").catch((err) =>{ console.log(err)});
-                interaction.member.setNickname(res.data[0]);
+                await interaction.member.setNickname(res.data[0]);
             });
         //interaction.followUp({ content: `lol`, ephemeral: true });
     }
