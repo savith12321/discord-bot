8d39543b54b83077372dedb0bc6afda8d6c7c559
savith nishitha <isavith.nishitha@gmail.com>
2021-09-22 15:34:48 +0530

    .
    .

--- SlashCommands/chess-together.js
+++ SlashCommands/chess-together.js
@@ -2,7 +2,7 @@ const { Client, CommandInteraction } = require("discord.js");
 const {SlashCommandBuilder} = require("@discordjs/builders");
 const  discordTogether = require("../Client/DiscordTogether");
 module.exports = {
-    ...new SlashCommandBuilder().setName("fishing-together").setDescription("play fishing.io whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch play fishing.io").setRequired(true)),
+    ...new SlashCommandBuilder().setName("chess-together").setDescription("play chess whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch play chess").setRequired(true)),
     run: async (client, interaction, args) => {
         const [channelID] = args;
         const channel = await client.channels.fetch(channelID).catch(() =>{});
