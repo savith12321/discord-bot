e31d27101194e1f0bc8ac9c1da2d6c4c4df2f30d
savith nishitha <isavith.nishitha@gmail.com>
2021-09-22 14:44:38 +0530

    .

--- SlashCommands/fishing-together.js
+++ SlashCommands/fishing-together.js
@@ -2,11 +2,11 @@ const { Client, CommandInteraction } = require("discord.js");
 const {SlashCommandBuilder} = require("@discordjs/builders");
 const  discordTogether = require("../Client/DiscordTogether");
 module.exports = {
-    ...new SlashCommandBuilder().setName("yt-together").setDescription("watch youtube whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch youtube").setRequired(true)),
+    ...new SlashCommandBuilder().setName("fishing-together").setDescription("play fishing.io whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch play fishing.io").setRequired(true)),
     run: async (client, interaction, args) => {
         const [channelID] = args;
         const channel = await client.channels.fetch(channelID).catch(() =>{});
         if(channel.type !== 'GUILD_VOICE') return interaction.followUp("please select a voice channel");
-        discordTogether.createTogetherCode(channelID, 'youtube').then((x) => interaction.followUp(x.code))
+        discordTogether.createTogetherCode(channelID, 'fishing').then((x) => interaction.followUp(x.code))
     }
 };
