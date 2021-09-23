bf92ca6a651a64e117a42e3bf64dbe32453077a5
savith nishitha <isavith.nishitha@gmail.com>
2021-09-23 12:27:30 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,14 +4,14 @@ module.exports = {
     description: "this is a ban command!",
     async execute(message, args){
       let error = false;
-      if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Don't wast your time bro **YOU DONT HAVE PERMS TO DO THAT IDIDOT**");
+      if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("Don't wast your time bro **YOU DONT HAVE PERMS TO DO THAT IDIDOT**");
       var member = message.mentions.users.first();
       if(member){
           var target = message.guild.members.cache.get(member.id);
           await target.ban().catch(async (err) => {
             error = true;
             await message.react("❌");
-            return message.channel.send(":boom: I could not ban that ban that member see do i have perms or he/she have higher perms thane me")
+            return message.channel.send(":boom: I could not ban that member see do i have perms or he/she have higher perms thane me")
           });          
           if(!error){
             message.reply(`You **KICK** ${target}`);
