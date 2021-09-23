43f30699b680cb01fefbf8ac82160f57c586ebc6
savith nishitha <isavith.nishitha@gmail.com>
2021-09-23 14:16:13 +0530

    .

--- SlashCommands/poker-together.js
+++ SlashCommands/poker-together.js
@@ -2,11 +2,11 @@ const { Client, CommandInteraction } = require("discord.js");
 const {SlashCommandBuilder} = require("@discordjs/builders");
 const  discordTogether = require("../Client/DiscordTogether");
 module.exports = {
-    ...new SlashCommandBuilder().setName("pocker-together").setDescription("play pocker whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch play pocker").setRequired(true)),
+    ...new SlashCommandBuilder().setName("poker-together").setDescription("play poker whith your friends").addChannelOption(option => option.setName("voice").setDescription("what channel you want to watch play poker").setRequired(true)),
     run: async (client, interaction, args) => {
         const [channelID] = args;
         const channel = await client.channels.fetch(channelID).catch(() =>{});
         if(channel.type !== 'GUILD_VOICE') return interaction.followUp("please select a voice channel");
-        discordTogether.createTogetherCode(channelID, 'pocker').then((x) => interaction.followUp(x.code))
+        discordTogether.createTogetherCode(channelID, 'poker').then((x) => interaction.followUp(x.code))
     }
 };
