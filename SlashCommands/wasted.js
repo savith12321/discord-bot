0617111482b42beff6508a9702a582df1afae360
savith nishitha <isavith.nishitha@gmail.com>
2021-09-23 13:40:31 +0530

    Create wasted.js

---
+++ SlashCommands/wasted.js
@@ -0,0 +1,15 @@
+const { Client, CommandInteraction } = require("discord.js");
+const {SlashCommandBuilder} = require("@discordjs/builders")
+module.exports = {
+    ...new SlashCommandBuilder().setName("wasted").setDescription("sends iamspeed pic with crop by the messages aouthors pfp").addUserOption(option => option.setName("user").setDescription("plaeace select a user").setRequired(true)),
+    run: async (client, interaction, args) => {
+        const user = interaction.options.getUser("user");
+        let link = `https://some-random-api.ml/canvas/wasted?avatar=${Member.user.displayAvatarURL({ format: "png" })}`
+        const attachment = new MessageAttachment(link, 'triggered.gif');
+        const embed = new MessageEmbed()
+          .setColor("RANDOM")
+          .setTimestamp()
+          .setImage('attachment://triggered.gif')
+        interaction.followUp({embeds:[embed], files: [attachment]});
+    }
+};
