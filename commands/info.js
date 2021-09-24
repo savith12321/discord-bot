c40d2bd880f14c25b509825e5171ceb16b2cf1d5
savith nishitha <isavith.nishitha@gmail.com>
2021-09-24 14:56:03 +0530

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -6,12 +6,12 @@ module.exports = {
     execute(message, args, client){
       colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
       const userEmbed = new Discord.MessageEmbed()
-      .setTitle("About me")
-      .addField("Made by:", "superN00b#7400, Naman#7653, SniproGamer#1045")
-      .addField("Server Count", `${client.guilds.cache.size}`)
-      .addField("Member Count", `${client.users.cache.size}`)
-      .addField("Ping", `${Math.round(client.ws.ping)}ms`)
-      .addField("Latency", `${Date.now() - message.createdTimestamp}`)
+      .setTitle("About Heat")
+      .addField("Made by:", "superN00b#7400, Naman#7653, SniproGamer#1045", true)
+      .addField("Server Count", `${client.guilds.cache.size}`, true)
+      .addField("Member Count", `${client.users.cache.size}`, true)
+      .addField("Ping", `${Math.round(client.ws.ping)}ms`, true)
+      .addField("Latency", `${Date.now() - message.createdTimestamp}ms`, true)
       .setThumbnail("https://media.discordapp.net/attachments/834658912422592553/835052092766158899/Untitled_design_1.png")
       .setFooter("Thanx for adding our bot c:")
       .setTimestamp()
