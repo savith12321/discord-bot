c6f00ed2379511de2999e3cd28f70371d388937e
superN00b <73011842+savith12321@users.noreply.github.com>
2021-09-27 14:47:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {Intents} = Discord = require('discord.js');// add this after Intents.FLAGS.GUILD_MEMBERS l
 const fs = require("fs");
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
 const config = require('./config');
 module.exports = client;
 client.commands = new Discord.Collection();
