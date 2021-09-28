702ed0e1fb1772a5e0dc54b26bc7c44c1924351f
savith nishitha <isavith.nishitha@gmail.com>
2021-09-28 15:35:02 +0530

    Create bird.js

---
+++ commands/bird.js
@@ -0,0 +1,26 @@
+const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const axios = require("axios");
+module.exports = {
+    name: 'bird',
+    cooldown: 3,
+    description: 'send a random bird image',
+    async execute (message, args, client){
+        colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
+        axios
+            .get('https://api.monkedev.com/attachments/monkey')
+            .then((res) => {
+                console.log("RES:", res.data[0].url)
+                const cat = res.data[0].url
+                const embed = new MessageEmbed()
+                    .setTitle("u like birds ?")
+                    .setImage(res.data[0].url)
+                    .setTimestamp()
+                    .setColor(colors[Math.floor(Math.random() * colors.length)])
+                    .setFooter("For bird lovers only")
+  
+                message.channel.send({embeds:[embed]});
+  
+            })
+  
+    }
+}
