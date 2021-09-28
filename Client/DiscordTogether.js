28106bca259ef5452e24f83ed8fb73e40b05a52f
savith nishitha <isavith.nishitha@gmail.com>
2021-09-28 09:51:37 +0530

    Update DiscordTogether.js

--- Client/DiscordTogether.js
+++ Client/DiscordTogether.js
@@ -1,4 +1,4 @@
-const DiscordTogether = require('discordtogether');
+const DiscordTogether = require('./discordtogether');
 const client = require('../index')
 
 const discordTogether = new DiscordTogether(client);
