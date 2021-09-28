b972dd8013a6b9f017178af509625264c3dd205d
savith nishitha <isavith.nishitha@gmail.com>
2021-09-28 15:48:52 +0530

    .

--- commands/monkey.js
+++ commands/monkey.js
@@ -1,22 +1,22 @@
 const Discord = { Client, MessageEmbed, MessageAttachment } = require('discord.js');
 const axios = require("axios");
 module.exports = {
-    name: 'bird',
+    name: 'monkey',
     cooldown: 3,
-    description: 'send a random bird image',
+    description: 'send a random monkey image',
     async execute (message, args, client){
         colors = ['#483d8b', '#7fff00', '#dc143c', '#ecf05b', '#41b1ed', '#e16ff2', '#f6a3cf', '#9cf063', '#32ecad']
         axios
-            .get('https://api.monkedev.com/attachments/bird')
+            .get('https://api.monkedev.com/attachments/monkey')
             .then((res) => {
                 console.log("RES:", res.data.url)
                 const cat = res.data.url
                 const embed = new MessageEmbed()
-                    .setTitle("u like birds ?")
+                    .setTitle("sorry i mistakenly sent u a image of u")
                     .setImage(res.data.url)
                     .setTimestamp()
                     .setColor(colors[Math.floor(Math.random() * colors.length)])
-                    .setFooter("For bird lovers only")
+                    .setFooter("For monkeys lovers only altho if ur a monkey")
   
                 message.channel.send({embeds:[embed]});
   
