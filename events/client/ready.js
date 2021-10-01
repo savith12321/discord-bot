652358e8a318bd6d21332b260045d230a999bb84
savith nishitha <isavith.nishitha@gmail.com>
2021-10-01 13:31:27 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -24,9 +24,9 @@ module.exports = async (_, client) => {
     console.log(`bot is ready ${servers} ${members}`);
     //await client.application.commands.set([]);
     //await client.application.commands.set(arrayOfSlashCommands);
-    //await client.guilds.cache
-    //.get("882850079533309962")
-    //.commands.set(arrayOfSlashCommands);
+    await client.guilds.cache
+    .get("893393632474824734")
+    .commands.set(arrayOfSlashCommands);
 
 
 }
