4549581e47256a341561543312bbc63423b41637
savith nishitha <isavith.nishitha@gmail.com>
2021-10-01 11:48:20 +0530

    .

---
+++ commands/door.js
@@ -0,0 +1,17 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const Meme = require("memer-api")
+const memer = new Meme('A0OrY8COhHj');
+module.exports = {
+    name: 'door',
+    cooldown: 0,
+    description: 'send a dor meme image',
+    async execute (message, args, client){
+        const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        memer.door(Member.user.displayAvatarURL({ format: "png" })).then(image => {
+            // This gives you a 'Buffer', for Discord to create an attachment
+            
+            var attachment = new Discord.MessageAttachment(image, "youtube.png");
+            message.channel.send(attachment)
+        })
+    }
+}
