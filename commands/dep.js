248a418c1b4c890456e9400cbf00c478787e0e49
savith nishitha <isavith.nishitha@gmail.com>
2021-10-03 13:47:50 +0530

    .

--- commands/dep.js
+++ commands/dep.js
@@ -10,8 +10,8 @@ module.exports = {
         await profileSchema.findOne({ UserID: message.author.id }, async (err, data) =>{
             if(data.wollet < args[0]) return message.reply("you dont have that much money on your wollet");
 
-            data.wollet = await data.wollet - args[0];
-            data.bank = await data.bank + args[0];
+            data.wollet =  data.wollet - args[0];
+            data.bank =  data.bank + args[0];
             data.save();
             message.reply(`${message.author}, now has ${data.wollet}€ on his wollet and ${data.bank}€ on his bank.`)
         });
