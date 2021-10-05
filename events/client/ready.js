16bce14ce84dbbbac4d1afbd2e443831433fc7b6
savith nishitha <isavith.nishitha@gmail.com>
2021-10-05 12:13:38 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -22,11 +22,11 @@ module.exports = async (_, client) => {
     var members = await client.users.cache.size;
     client.user.setActivity(`[lol help] | Watching ${servers} servers | v1.1`, { type: "LISTENING" });
     console.log(`bot is ready ${servers} ${members}`);
-    //await client.application.commands.set([]);
+   // await client.application.commands.set([]);
     //await client.application.commands.set(arrayOfSlashCommands);
-//    await client.guilds.cache
-//    .get("893393632474824734")
-//    .commands.set(arrayOfSlashCommands);
+   await client.guilds.cache
+    .get("894118785492066354")
+    .commands.set(arrayOfSlashCommands);
 
 
 }
