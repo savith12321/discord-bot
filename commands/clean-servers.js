348563cc6a5d90bf82e97e33aa4bd5d2f5c28a78
savith nishitha <isavith.nishitha@gmail.com>
2021-10-06 13:45:59 +0530

    .

--- commands/clean-servers.js
+++ commands/clean-servers.js
@@ -3,7 +3,7 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'clean-servers',
     cooldown: 0,
-    description: '',
+    description: 'clean the bad/fake servers',
     async execute (message, args, client){
         if(!message.author.id == "856767606869458946") return message.reply(":warning: this is a owner only command")
         let final_message = ""
