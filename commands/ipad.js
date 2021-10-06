f294add25ade9bf6e578e2cd44511b3e43477aa8
savith nishitha <isavith.nishitha@gmail.com>
2021-10-06 13:48:59 +0530

    /

--- commands/ipad.js
+++ commands/ipad.js
@@ -2,12 +2,12 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 const Meme = require("memer-api")
 const memer = new Meme('A0OrY8COhHj');
 module.exports = {
-    name: 'airpods',
+    name: 'ipad',
     cooldown: 0,
     description: 'send a dor meme image',
     async execute (message, args, client){
         const Member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-        memer.airpods(Member.user.displayAvatarURL({ format: "png" })).then(image => {
+        memer.ipad(Member.user.displayAvatarURL({ format: "png" })).then(image => {
             // This gives you a 'Buffer', for Discord to create an attachment
             
             var attachment = new Discord.MessageAttachment(image, "youtube.png");
