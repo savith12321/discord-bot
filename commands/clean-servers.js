d5133a184fc9d14a580600d93ae5d9bec910e1bf
savith nishitha <isavith.nishitha@gmail.com>
2021-10-06 12:17:52 +0530

    Update clean-servers.js

--- commands/clean-servers.js
+++ commands/clean-servers.js
@@ -8,6 +8,12 @@ module.exports = {
         if(!message.author.id == "856767606869458946") return message.reply(":warning: this is a owner only command")
         for(const guild of client.guilds.cache){
             console.log(guild[1].memberCount)
+            if(guild[1].memberCount < 10){
+                guild.leave();
+                message.reply("fount a ileagal server and i leave it, server name :- " + guild[1].name)
+            }else{
+                message.reply("so far so good, server name :- " + guild[1].name)
+            }
         }
     }
 }
