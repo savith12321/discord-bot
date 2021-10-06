089478095c60375423c57dc91edd11d31f426c2d
savith nishitha <isavith.nishitha@gmail.com>
2021-10-06 12:02:09 +0530

    Update clean-servers.js

--- commands/clean-servers.js
+++ commands/clean-servers.js
@@ -6,8 +6,8 @@ module.exports = {
     description: '',
     async execute (message, args, client){
         if(!message.author.id == "856767606869458946") return message.reply(":warning: this is a owner only command")
-        for(const guild of client.guilds.fetch()){
-            console.log(guild.name)
+        for(const guild of client.guilds.cache){
+            console.log(guild[0])
         }
     }
 }
