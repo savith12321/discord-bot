515e8a3cad0c3e20c1bd75fb23165e5b9bc66d13
savith nishitha <isavith.nishitha@gmail.com>
2021-10-06 10:55:33 +0530

    Create clean-servers.js

---
+++ commands/clean-servers.js
@@ -0,0 +1,13 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'clean-servers',
+    cooldown: 0,
+    description: '',
+    async execute (message, args, client){
+        if(!message.author.id == "856767606869458946") return message.reply(":warning: this is a owner only command")
+        for(guild in client.guilds){
+            console.log(guild)
+        }
+    }
+}
